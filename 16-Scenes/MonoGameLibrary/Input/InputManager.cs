b952543693b8f42342641d16658a22a6aad96928
Christopher Whitley <103014489+AristurtleDev@users.noreply.github.com>
2025-03-13 14:52:05 -0400

    scenes wip

--- 16-Scenes/MonoGameLibrary/Input/InputManager.cs
+++ 16-Scenes/MonoGameLibrary/Input/InputManager.cs
@@ -2,7 +2,7 @@ using Microsoft.Xna.Framework;
 
 namespace MonoGameLibrary.Input;
 
-public class InputManager : GameComponent, IInputManager
+public class InputManager
 {
     /// <summary>
     /// Gets the state information of keyboard input.
@@ -23,12 +23,7 @@ public class InputManager : GameComponent, IInputManager
     /// Creates a new InputManager.
     /// </summary>
     /// <param name="game">The game this input manager belongs to.</param>
-    public InputManager(Game game) : base(game) { }
-
-    /// <summary>
-    /// Initializes this input manager.
-    /// </summary>
-    public override void Initialize()
+    public InputManager()
     {
         Keyboard = new KeyboardInfo();
         Mouse = new MouseInfo();
@@ -38,16 +33,13 @@ public class InputManager : GameComponent, IInputManager
         {
             GamePads[i] = new GamePadInfo((PlayerIndex)i);
         }
-
-        // Add this instance to the game's services container.
-        Game.Services.AddService<IInputManager>(this);
     }
 
     /// <summary>
     /// Updates the state information for the keyboard, mouse, and gamepad inputs.
     /// </summary>
     /// <param name="gameTime">A snapshot of the timing values for the current frame.</param>
-    public override void Update(GameTime gameTime)
+    public void Update(GameTime gameTime)
     {
         Keyboard.Update();
         Mouse.Update();
