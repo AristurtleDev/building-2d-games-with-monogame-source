cbd09844344dc7e3647e50574f319a93e0285070
Christopher Whitley <103014489+AristurtleDev@users.noreply.github.com>
2025-03-14 17:03:33 -0400

    wip

--- 17-RenderTarget2D/MonoGameLibrary/Scenes/Scene.cs
+++ 17-RenderTarget2D/MonoGameLibrary/Scenes/Scene.cs
@@ -8,16 +8,6 @@ namespace MonoGameLibrary.Scenes;
 public abstract class Scene : IDisposable
 {
     /// <summary>
-    /// Gets a reference to the current game instance.
-    /// </summary>
-    protected Game Game { get; }
-
-    /// <summary>
-    /// Gets a reference to the scene manager this scene belongs to.
-    /// </summary>
-    protected ISceneManager SceneManager { get; }
-
-    /// <summary>
     /// Gets the ContentManager used for loading scene-specific assets.
     /// </summary>
     /// <remarks>
@@ -26,50 +16,26 @@ public abstract class Scene : IDisposable
     protected ContentManager Content { get; }
 
     /// <summary>
-    /// Gets the graphics device manager used to manage the presentation of graphics.
-    /// </summary>
-    protected GraphicsDeviceManager GraphicsDeviceManager { get; }
-
-    /// <summary>
-    /// Gets a reference to the GraphicsDevice used for rendering.
-    /// </summary>
-    protected GraphicsDevice GraphicsDevice => GraphicsDeviceManager.GraphicsDevice;
-
-    /// <summary>
     /// Gets a value that indicates if the scene has been disposed of.
     /// </summary>
     public bool IsDisposed { get; private set; }
 
     /// <summary>
+    /// Gets the render target this scene renders too.
+    /// </summary>
+    public RenderTarget2D RenderTarget { get; protected set; }
+
+    /// <summary>
     /// Creates a new scene instance.
     /// </summary>
-    /// <param name="game">The game instance</param>
-    public Scene(Game game)
+    public Scene()
     {
-        Game = game;
-
-        // Get the GraphicsDeviceManager from the game's services container
-        IGraphicsDeviceManager deviceManager = Game.Services.GetService<IGraphicsDeviceManager>();
-
-        // Ensure the service was available.
-        if (deviceManager != null)
-        {
-            GraphicsDeviceManager = (GraphicsDeviceManager)deviceManager;
-        }
-        else
-        {
-            throw new InvalidOperationException("Game does not contain a graphics device service");
-        }
-
-        // Get the ISceneManager service from the game's service container
-        SceneManager = Game.Services.GetService<ISceneManager>();
-
         // Create a content manager for the scene
-        Content = new ContentManager(game.Services);
+        Content = new ContentManager(Core.Instance.Services);
 
         // Set the root directory for content to the same as the root directory
         // for the game's content.
-        Content.RootDirectory = game.Content.RootDirectory;
+        Content.RootDirectory = Core.Instance.Content.RootDirectory;
     }
 
     // Finalizer, called when object is cleaned up by garbage collector.
@@ -90,7 +56,12 @@ public abstract class Scene : IDisposable
     /// <summary>
     /// Override to provide logic to load content for the scene.
     /// </summary>
-    public virtual void LoadContent() { }
+    public virtual void LoadContent()
+    {
+        // Since a render target is a graphical resource, we'll generate it
+        // during content loading.
+        GenerateRenderTarget();
+    }
 
     /// <summary>
     /// Unloads scene-specific content.
@@ -109,9 +80,34 @@ public abstract class Scene : IDisposable
     /// <summary>
     /// Draws this scene.
     /// </summary>
-    /// <param name="spriteBatch">The sprite batch used for rendering.</param>
     /// <param name="gameTime">A snapshot of the timing values for the current frame.</param>
-    public virtual void Draw(SpriteBatch spriteBatch, GameTime gameTime) { }
+    public virtual void Draw(GameTime gameTime) { }
+
+    /// <summary>
+    /// Generates a RenderTarget2D instance for the scene to render to.
+    /// </summary>
+    protected virtual void GenerateRenderTarget()
+    {
+        // Get a reference to the graphics device
+        GraphicsDevice graphicsDevice = Core.Instance.GraphicsDevice;
+
+        // Determine the width of the render target based on the width of the back buffer.
+        int width = graphicsDevice.PresentationParameters.BackBufferWidth;
+
+        // Determine the height of the render target based on the height of the back buffer.
+        int height = graphicsDevice.PresentationParameters.BackBufferHeight;
+
+        //  If the RenderTarget instance has already been created previously but has yet
+        //  to be disposed of properly, dispose of the instance before setting a new one.
+        if (RenderTarget != null && !RenderTarget.IsDisposed)
+        {
+            RenderTarget.Dispose();
+        }
+
+        // Generate the RenderTarget
+        RenderTarget = new RenderTarget2D(graphicsDevice, width, height);
+
+    }
 
     /// <summary>
     /// Disposes of this scene.
@@ -140,6 +136,11 @@ public abstract class Scene : IDisposable
         {
             UnloadContent();
             Content.Dispose();
+
+            if(RenderTarget != null && !RenderTarget.IsDisposed)
+            {
+                RenderTarget.Dispose();
+            }
         }
     }
 }
---
+++ 17-RenderTarget2D/MonoGameLibrary/Scenes/SceneTransitionKind.cs
@@ -0,0 +1,14 @@
+namespace MonoGameLibrary.Scenes;
+
+public enum SceneTransitionKind
+{
+    /// <summary>
+    /// The scene is transitioning in.
+    /// </summary>
+    In,
+
+    /// <summary>
+    /// The scene is transitioning out.
+    /// </summary>
+    Out
+}
